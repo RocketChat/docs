f7c11024fcf2aef3ea2010edfaeb6f55c19a52ef
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2023-09-19 19:45:22 -0300

    Small improvements to releases-support image generator

--- .scripts/generateVersionImage.mjs
+++ .scripts/generateVersionImage.mjs
@@ -4,51 +4,56 @@ import D3Node from 'd3-node';
 import Svg2png from 'svg2png';
 
 const styles = `
-.current {
-	fill: #fbcd94;
+.latest {
+	fill: #FFD031;
+	stroke: #FFD031;
+	fill-opacity: 0.5;
+	stroke-width: 2;
+	stroke-dasharray: 2,2;
 }
 .active {
-	fill: #a5eb9a;
-	stroke: #73BF67;
-}
-.active-lts {
-	fill: #e69090;
+	fill: #96F0D2;
 }
 .maintenance {
-	fill: #73BF67;
+	fill: #2DE0A5;
 }
-.lts {
-	fill: #BF6767;
+.active-lts {
+	fill: #F98F9D;
+}
+.maintenance-lts {
+	fill: #F5455C;
+}
+.label {
+	fill: #fff;
+	font: 18px sans-serif;
+	font-weight: 100;
+	dominant-baseline: middle;
+}
+.label-lts {
+
+}
+.label-latest {
+
+}
+.label-version {
+	fill: #cecece;
 }
 .tick text {
 	font: 14px sans-serif;
-	fill: #89a19d;
+	fill: #cecece;
 }
-.axis--y .tick text {
-	text-anchor: end;
+.tick line {
+	stroke: #cecece;
+	stroke-dasharray: 4,4;
 }
-.label {
-	fill: #305d2a;
-	font: 18px sans-serif;
-	font-weight: 100;
-	text-anchor: start;
-	dominant-baseline: middle;
-	text-transform: uppercase;
-}
-.label-lts {
-	fill: #693030;
-}
-.label-current {
-	fill: #6d491b;
-}
-#bar-container {
-	background-color: red;
+.background {
+	fill: #1D74F5;
 }
 `;
 
 
 export function create(data, options = {}) {
-	let { queryStart, queryEnd, html, svg: svgFile, png = './releases-support.png', animate, excludeMaster, projectName, margin: marginInput } = options;
+	let { queryStart, queryEnd, png = './releases-support.png' } = options;
 	if (!queryStart) {
 		queryStart = new Date();
 		queryStart.setMonth(queryStart.getMonth() - 12);
@@ -66,10 +71,8 @@ export function create(data, options = {}) {
 	});
 	data.reverse();
 
-	// const data = parseInput(options.data, queryStart, queryEnd, excludeMaster, projectName);
-
-	const d3n = new D3Node({ svgStyles: styles, d3Module: D3 });
-	const margin = marginInput || { top: 40, right: 40, bottom: 20, left: 50 };
+	const d3n = new D3Node({ styles: styles, d3Module: D3 });
+	const margin = { top: 40, right: 50, bottom: 20, left: 50 };
 	const width = 960 - margin.left - margin.right;
 	const height = 500 - margin.top - margin.bottom;
 
@@ -90,14 +93,13 @@ export function create(data, options = {}) {
 		.attr('height', height + margin.top + margin.bottom);
 
 	baseSvg.append('rect')
-		.attr('fill', 'black')
+		.attr('class', 'background')
 		.attr('width', '100%')
 		.attr('height', '100%');
 
 	const svg = baseSvg.append('g')
 		.attr('id', 'bar-container')
 		.attr('transform', `translate(${margin.left}, ${margin.top})`)
-
 
 	function calculateWidth(data) {
 		return xScale(data.release.extendedSupport?.end || queryEnd) - xScale(data.release.releasedAt);
@@ -110,51 +112,49 @@ export function create(data, options = {}) {
 	function customXAxis(g) {
 		g.call(xAxis);
 		g.select('.domain').remove();
-		g.selectAll('.tick:nth-child(odd) line').attr('stroke', '#89a19d');
-		g.selectAll('.tick:nth-child(even) line')
-			.attr('stroke', '#89a19d')
-			.attr('stroke-dasharray', '2,2');
-		g.selectAll('.tick text').attr('y', 0).attr('dy', -10);
+		g.selectAll('.tick text').attr('y', -20);
 	}
 
 	function customYAxis(g) {
 		g.call(yAxis);
 		g.select('.domain').remove();
-		g.selectAll('.tick line').attr('stroke', '#e1e7e7');
-		g.selectAll('.tick text').attr('x', 0).attr('dx', -10);
-		g.append('line')
-			.attr('y1', height)
-			.attr('y2', height)
-			.attr('x2', width)
-			.attr('stroke', '#89a19d');
+		g.selectAll('.tick').remove();
 	}
 
 	svg.append('g')
-		.attr('class', 'axis axis--x')
 		.call(customXAxis);
 
 	svg.append('g')
-		.attr('class', 'axis axis--y')
 		.call(customYAxis);
 
 	const bar = svg.selectAll('#bar-container').data(data).enter().append('g');
 
 	bar.append('rect')
-		.attr('class', (data) => { return `bar-join ${data.release.extendedSupport ? (data.release.lts ? 'active-lts' : 'active') : 'current'}`; })
+		.attr('class', (data) => { return `bar-join ${data.release.extendedSupport ? (data.release.lts ? 'active-lts' : 'active') : 'latest'}`; })
 		.attr('x', (data) => { return xScale(data.release.releasedAt); })
 		.attr('y', (data) => { return yScale(data.release.version); })
 		.attr('width', (data) => {return xScale((data.release.extendedSupport?.end || queryEnd)) - xScale(data.release.releasedAt)})
 		.attr('height', (data) => calculateHeight(data));
 
 	bar.append('rect')
-		.attr('class', (data) => { return `bar ${data.release.extendedSupport ? (data.release.lts ? 'lts' : 'maintenance') : ''}`; })
-		.attr('x', (data) => { return xScale(data.release.extendedSupport?.start) + 2; })
+		.attr('class', (data) => { return `bar ${data.release.extendedSupport ? (data.release.lts ? 'maintenance-lts' : 'maintenance') : ''}`; })
+		.attr('x', (data) => { return data.release.extendedSupport?.start <= queryStart ? 0 : xScale(data.release.extendedSupport?.start) + 2; })
 		.attr('y', (data) => { return yScale(data.release.version) + 2; })
-		.attr('width', (data) => { return xScale(data.release.extendedSupport?.end || queryEnd) - xScale(data.release.extendedSupport?.start) - 4})
+		.attr('width', (data) => { return xScale(data.release.extendedSupport?.end || queryEnd) - xScale(data.release.extendedSupport?.start) - (data.release.extendedSupport?.start <= queryStart ? 2 : 4)})
 		.attr('height', (data) => calculateHeight(data) - 4);
 
 	bar.append('text')
-		.attr('class', (data) => { return `bar ${data.release.extendedSupport ? (data.release.lts ? 'label label-lts' : 'label') : 'label label-current'}`; })
+		.attr('class', 'bar label label-version')
+		.attr('x', (data) => {
+			return xScale(data.release.releasedAt) - (data.release.version.length * 10);
+		})
+		.attr('y', (data) => {
+			return yScale(data.release.version) + (calculateHeight(data)/2) + 1;
+		})
+		.text((data) => { return data.release.version })
+
+	bar.append('text')
+		.attr('class', (data) => { return `bar label ${data.release.extendedSupport ? (data.release.lts ? 'label-lts' : '') : 'label-latest'}`; })
 		.attr('x', (data) => {
 			return xScale(data.release.extendedSupport?.start || data.release.releasedAt) + 10;
 		})
@@ -165,7 +165,7 @@ export function create(data, options = {}) {
 		.text((data) => { return data.support; })
 		.style('opacity', (data) => {
 			// Hack to deal with overflow text.
-			const min = data.support.length * 14;
+			const min = data.support.length * 10;
 			return +(calculateWidth(data) >= min);
 		});
 
